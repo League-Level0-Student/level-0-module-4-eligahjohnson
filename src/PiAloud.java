f061f548ea13f28de9cba437125bbbd30f08d98e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-13 19:21:31 -0800

    PiAloud Sleephead

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -8,11 +8,19 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
+		
+	
+    
+	
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String frog = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+System.out.println(frog.charAt(0));
+for (int i = 0; i < frog.length(); i++) {
+System.out.println(frog.charAt(i));	
+speak( frog.charAt(i));
+}
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
 	// 5. Use the speak() method to speak all the digits of Pi.
@@ -21,7 +29,7 @@ public class PiAloud {
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -17,8 +17,18 @@ public class SleepyHead {
          * JOptionPane.YES_NO_OPTION);
          */
     
-
-
+     int king = JOptionPane.showConfirmDialog(null, "is a week day?");
+     if(king==0) {
+    	 int vacation = JOptionPane.showConfirmDialog(null, "is vacation??");
+    	 if(vacation==0) {
+    		 JOptionPane.showMessageDialog(null, "sleep in");
+    	 }
+    	 else	 JOptionPane.showMessageDialog(null, "Get up LazyBone!");
+     }
+     if(king==1) {
+    	 JOptionPane.showMessageDialog(null, "Sleep in ");
+     }
+     
         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
