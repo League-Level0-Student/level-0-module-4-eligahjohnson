cc960499b063006a7be05b6df0b1a27f55a82b87
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-27 19:23:08 -0800

    Moduele4checkpointip

--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -7,18 +7,19 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String taco = "buTtfArT";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(taco.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+System.out.println(taco.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+		System.out.println(taco.substring(0, 3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(taco.charAt(4));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(taco.substring(5,6+2));
 	}
 }
