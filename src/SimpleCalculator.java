4f3705c927bfaf60044bcc3eab52b71ba23e4877
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-13 19:20:42 -0700

    -Moudle3checkpoint

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -6,12 +6,12 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-    String frog = JOptionPane.showInputDialog("Put 2 numbers of your choose");
-    Parse.int 
-		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
-				null);
+//    String frog = JOptionPane.showInputDialog("Put 2 numbers of your choose");
+//    Parse.int 
+//		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
+//		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
+//				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+//				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
 
